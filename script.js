da3cbd67015d59c96256149de5bdb326f9f52138
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-07-12 13:30:39 -0600

    final code js 104

--- script.js
+++ script.js
@@ -28,6 +28,65 @@ const restaurant = {
   },
 
   order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '20:00',
+    address,
+  }) {
+    console.log(
+      `order received! ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+};
+
+restaurant.orderDelivery({
+  time: '22, 30',
+  address: 'Via de sole, 21',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+restaurant.orderDelivery({
+  address: 'via del sole, 21',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName, hours, tags);
+
+// DEFAULT VALUES
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+
+// MUTATING VARIABLES
+
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+
+({ a, b } = obj);
+console.log(a, b);
+
+// NESTED OBJECTS
+const {
+  fri: { open: o, close: c },
+} = openingHours;
+console.log(o, c);
+
+/*===========  DESTRUCTURING OBJECTS ===============/*
+  order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterindex], this.mainMenu[mainindex]];
   },
 };
@@ -65,3 +124,4 @@ console.log(i, j, k);
 
 const [p, q, r] = [8, 9];
 console.log(p, q, r);
+*/

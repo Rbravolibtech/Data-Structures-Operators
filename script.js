8fee8e7a6dff792438afa875bba4698c202cece6
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-07-12 15:04:12 -0600

    done with code 106

--- script.js
+++ script.js
@@ -46,8 +46,50 @@ const restaurant = {
     console.log(`Here is your delicious pasta with 
     ${ing1}, ${ing2}, ${ing3}`);
   },
+
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
 };
 
+//DESTRUCTURING
+
+// SPREAD, BECAUSE ON RIGHT SIDE OF =
+const arr = [1, 2, ...[3, 4]];
+
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log(a, b, others);
+
+const [pizza, , risotto, ...otherFood] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+console.log(pizza, risotto, otherFood);
+
+//OBJECTS
+
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(weekdays);
+
+// FUNCTIONS
+
+const add = function (...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+  numbers[i];
+  console.log(sum);
+};
+add(2, 3);
+add(5, 3, 7, 2);
+add(8, 2, 5, 3, 2, 1, 4);
+
+const x = [23, 5, 7];
+add(...x);
+
+restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
+restaurant.orderPizza('mushrooms');
+/*=========== REST PATTERN AND PARAMETERS ===============/*
 const arr = [7, 8, 9];
 const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
 console.log(badNewArr);

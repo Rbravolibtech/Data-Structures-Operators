db319b0e3498e7059e61754be619e7ad248e2895
Rbravolibtech <122310916+Rbravolibtech@users.noreply.github.com>
2023-07-17 13:38:04 -0600

    maps fundamentals

--- script.js
+++ script.js
@@ -51,48 +51,79 @@ const restaurant = {
     console.log(otherIngredients);
   },
 };
+
+rest.set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic']);
+
+/*==============MAPS: FUNDAMENTALS============== */
+const rest = new Map();
+rest.set('name', 'Classico Italiano');
+rest.set(1, 'Firenze, Italy');
+console.log(rest.set(2, 'Lisbon, Portugal'));
+rest
+  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+  .set('open', 11)
+  .set('close', 23)
+  .set(true, 'We are open :D')
+  .set(false, 'We are closed :(');
+
+console.log(rest.get('name'));
+console.log(rest.get(true));
+
+console.log(rest.get(1));
+const time = 8;
+console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+console.log(rest.has('categories'));
+rest.delete(2);
+// rest.clear();
+const arr = [1, 2];
+rest.set(arr, 'Test');
+rest.set(document.querySelector('h1'), 'Heading');
+console.log(rest);
+console.log(rest.size);
+console.log(rest.get(arr));
+
 /*==============SETS ============== */
 
-const ordersSet = new Set([
-  'PasTa',
-  'PizzA',
-  'Spaghetti',
-  'Alfredo',
-  'RisoTTO',
-  'CheESE',
-]);
-console.log(ordersSet);
+// const ordersSet = new Set([
+//   'PasTa',
+//   'PizzA',
+//   'Spaghetti',
+//   'Alfredo',
+//   'RisoTTO',
+//   'CheESE',
+// ]);
+// console.log(ordersSet);
 
-console.log(new Set('Bravo'));
+// console.log(new Set('Bravo'));
 
-console.log(ordersSet.size);
-console.log(ordersSet.has('cheESE'));
-console.log(ordersSet.has('Alfredo'));
-ordersSet.add('Garlic Bread');
-ordersSet.add('cheese Bread');
-ordersSet.delete;
-//ordersSet.clear();
-console.log(ordersSet);
+// console.log(ordersSet.size);
+// console.log(ordersSet.has('cheESE'));
+// console.log(ordersSet.has('Alfredo'));
+// ordersSet.add('Garlic Bread');
+// ordersSet.add('cheese Bread');
+// ordersSet.delete;
+// //ordersSet.clear();
+// console.log(ordersSet);
 
-for (const order of ordersSet) console.log(order);
+// for (const order of ordersSet) console.log(order);
 
-//EXAMPLE
-const staff = [
-  'waiter',
-  'dishwasher',
-  'chef',
-  'manager',
-  'bartender',
-  'greeter',
-];
-const staffUnique = [...new Set(staff)];
-console.log(staffUnique);
+// //EXAMPLE
+// const staff = [
+//   'waiter',
+//   'dishwasher',
+//   'chef',
+//   'manager',
+//   'bartender',
+//   'greeter',
+// ];
+// const staffUnique = [...new Set(staff)];
+// console.log(staffUnique);
 
-console.log(
-  new Set(['waiter', 'dishwasher', 'chef', 'manager', 'bartender', 'greeter'])
-    .size
-);
-console.log(new Set('RobertoBravoJr'.size));
+// console.log(
+//   new Set(['waiter', 'dishwasher', 'chef', 'manager', 'bartender', 'greeter'])
+//     .size
+// );
+// console.log(new Set('RobertoBravoJr'.size));
 
 /*==============CODING CHALLENGE 2 ============== */
 // Let's continue with our football betting app!
